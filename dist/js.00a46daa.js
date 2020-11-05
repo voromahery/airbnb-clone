a1fbf795feb5042feafa39303bbf751544c8bd4a
voromahery <daniel.her@onja.org>
2020-11-05 07:28:40 +0300

    Add a new icon in the decrement button

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -28797,7 +28797,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64740" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50471" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
